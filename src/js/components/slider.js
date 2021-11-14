5b761b2a084974cd5778da2c7436bbfb10db9143
vladvlas <vladvlas175@gmail.com>
2021-11-14 17:29:55 +0300

    added slider

---
+++ src/js/components/slider.js
@@ -0,0 +1,44 @@
+const slider = document.querySelector('.slider-main')
+const sliderNav = document.querySelector('.slider-nav')
+
+// const swiper = new Swiper(slider, {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+
+let mySwiperNav = new Swiper (sliderNav, {
+    slidesPerView: 3,
+    spaceBetween: 15,
+    freeMode: true,
+    loop: true,
+    direction: 'vertical',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+let mySwiper = new Swiper(slider, {
+    spaceBetween: 10,
+    thumbs: {
+        swiper: mySwiperNav,
+    },
+    effect: 'fade'
+})
